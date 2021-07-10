71eac9486f9c6e686ff0d0ca4d289f493b35e1df
LiiNen <38001238+LiiNen@users.noreply.github.com>
2021-07-10 15:46:19 +0900

    Update App.java

--- src/main/java/kr/ac/ajou/coding/App.java
+++ src/main/java/kr/ac/ajou/coding/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
 //         System.out.println( "Hello World!" );
-        System.out.println( "Test build4!" );
+        System.out.println( "Test build5!" );
     }
 }
